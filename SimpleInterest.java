0fba4da24cb1c600d12d6ab6496241bf32bbb738
Raghavendra0088 <raghavendrag0088@gmail.com>
2026-01-30 10:17:51 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,6 +1,6 @@
 class SimpleInterest {
     public static void main(String[] args) {
-        int p = 1000, r = 10, t = 4;
+        int p = 1000, r = 20, t = 4;
         int si = (p * r * t) / 100;
         System.out.println("Simple Interest = " + si);
     }
